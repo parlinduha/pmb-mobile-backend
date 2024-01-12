131614c3d09f6c48f58c0862e8d272f5586c8daf
ParlinD <duhaperi@gmail.com>
2024-01-12 16:37:52 +0700

    update backend allowed

--- pages/api/login/index.ts
+++ pages/api/login/index.ts
@@ -1,20 +1,24 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import Cors from "micro-cors";
 import fs from "fs";
 import path from "path";
 
+const cors = Cors({
+  allowMethods: ["POST"],
+  origin: "https://pmb-mobile-backend.vercel.app", // Update with the actual origin of your Angular app
+});
+
 const filePath = path.join(process.cwd(), "data", "students.json");
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
-    const { email, password } = req.body;
-
     try {
       const students = JSON.parse(fs.readFileSync(filePath, "utf-8"));
 
       // Find student by email
-      const student = students.find((s: any) => s.email === email);
+      const student = students.find((s: any) => s.email === req.body.email);
 
-      if (!student || student.password !== password) {
+      if (!student || student.password !== req.body.password) {
         // If student not found or password doesn't match, return an error
         return res.status(401).json({ error: "Invalid email or password" });
       }
@@ -34,3 +38,5 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     return res.status(405).json({ error: "Method Not Allowed" });
   }
 };
+
+export default cors(handler);
