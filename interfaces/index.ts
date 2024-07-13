1d7299a08282ca99149d021c665af3c1a519b6ae
ParlinD <duhaperi@gmail.com>
2024-07-13 23:58:40 +0700

    update interface data person

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,37 +1,14 @@
 export type Person = {
-  id: string;
-  major1: string;
-  major2?: string;
-  wavePeriod: string;
-  class: string;
-  fullName: string;
-  gender: string;
-  birthPlace: string;
-  birthDate: string;
-  homePhoneAreaCode?: string;
-  homePhoneNumber?: string;
-  mobileNumber: string;
-  fullAddress: string;
-  subDistrict: string;
-  district: string;
-  city: string;
-  province: string;
-  email: string;
-  graduationYear?: string;
-  diplomaNumber?: string;
-  schoolOrigin: string;
-  religion: string;
-  fatherName: string;
-  fatherAddress?: string;
-  fatherOccupation: string;
-  fatherMobileNumber?: string;
-  motherName: string;
-  motherAddress?: string;
-  motherOccupation: string;
-  motherMobileNumber?: string;
-  paymentProof: string;
+  id: string
+  name: string
+  height: string
+  mass: string
+  hair_color: string
+  skin_color: string
+  eye_color: string
+  gender: string
 }
 
 export type ResponseError = {
-  message: string;
+  message: string
 }
