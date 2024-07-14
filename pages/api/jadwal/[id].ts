8638df3495b3773d3e3d979f88d4b2e722bdf76d
ParlinD <duhaperi@gmail.com>
2024-07-14 20:27:14 +0700

    update build 1

--- pages/api/jadwal/[id].ts
+++ pages/api/jadwal/[id].ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { JadwalBiaya } from '../../../interfaces';
-import { readData, writeData } from '../../../utils/fsUtils';
+import { readData, writeData } from '../../../utils/jadwal';
 import Cors from 'nextjs-cors';
 
 export default async function handler(
@@ -11,24 +11,31 @@ export default async function handler(
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
     origin: "*",
   });
+
+  // Baca data terlebih dahulu
   let informationData = await readData();
   const { id } = req.query;
-  const infoIndex = informationData.findIndex((info:any) => info.id === id);
-
-  if (infoIndex === -1) {
-    return res.status(404).json({ message: `Information with id: ${id} not found.` });
-  }
+  const infoIndex = informationData.findIndex((info: any) => info.id === id);
 
   if (req.method === 'GET') {
+    if (infoIndex === -1) {
+      return res.status(404).json({ message: `Information with id: ${id} not found.` });
+    }
     // Get information by id
     res.status(200).json(informationData[infoIndex]);
   } else if (req.method === 'PUT') {
+    if (infoIndex === -1) {
+      return res.status(404).json({ message: `Information with id: ${id} not found.` });
+    }
     // Update information by id
     const updatedInfo: JadwalBiaya = { ...informationData[infoIndex], ...req.body };
     informationData[infoIndex] = updatedInfo;
     await writeData(informationData);
     res.status(200).json(updatedInfo);
   } else if (req.method === 'DELETE') {
+    if (infoIndex === -1) {
+      return res.status(404).json({ message: `Information with id: ${id} not found.` });
+    }
     // Delete information by id
     const deletedInfo = informationData.splice(infoIndex, 1);
     await writeData(informationData);
